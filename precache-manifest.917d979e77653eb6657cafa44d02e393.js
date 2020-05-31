9eb0fc73392ad0eb856638db179fff935a653187
unknown <mido.elnaggar@gmail.com>
2020-05-31 13:48:24 +0200

    Updates

---
+++ precache-manifest.917d979e77653eb6657cafa44d02e393.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "913ec19148e2b1c57e362806703241a7",
+    "url": "/inventory_fe/index.html"
+  },
+  {
+    "revision": "cd9c595a4d2b09e47d64",
+    "url": "/inventory_fe/static/css/2.6d92adba.chunk.css"
+  },
+  {
+    "revision": "cd9c595a4d2b09e47d64",
+    "url": "/inventory_fe/static/js/2.d7cbfe12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/inventory_fe/static/js/2.d7cbfe12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4f65959f579f3a62a0c7",
+    "url": "/inventory_fe/static/js/main.641513a2.chunk.js"
+  },
+  {
+    "revision": "da1e17044a2cf28bedb2",
+    "url": "/inventory_fe/static/js/runtime-main.afe2a512.js"
+  }
+]);
